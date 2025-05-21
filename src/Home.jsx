e1b332b9fa35e2f69caa58141a16e74ce4e2c310
JoaoVitor365 <rm94269@estudante.fieb.edu.br>
2025-05-21 11:22:41 +0000

    Grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,15 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
-
+import PokeGride from './PokeGrid'
+import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
 const Home = () => {
 
     return (
         <div>
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
+            <PokeGrid />
+            <PokeFlex />
         </div>
     )
 }
